f782ecf181b299e225277184eb030201a4f65baa
ioop987 <ioop987987@gmail.com>
2023-07-14 20:23:26 +0800

    Merge branch 'dev' of https://github.com/polikuj1/vueCart-teamwork into dev
    # Conflicts:
    #	src/views/HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+<<<<<<< HEAD
     <div class="card">
       <div class="pic">
         <img src="https://picsum.photos/300/200/?random=10">
@@ -8,6 +9,9 @@
         <router-link to="/comic">go shopping now</router-link>
       </div>
     </div>
+=======
+    <Services></Services>
+>>>>>>> a56af9c1fef9c1f47a1e40c90e1520684e5d0026
   </div>
 </template>
 
@@ -15,6 +19,12 @@
 
 
 </script>
+<<<<<<< HEAD
 <style lang="scss" scoped>
 @import '@/assets/scss/page/home.scss';
 </style>
+=======
+123
+456
+789
+>>>>>>> a56af9c1fef9c1f47a1e40c90e1520684e5d0026
