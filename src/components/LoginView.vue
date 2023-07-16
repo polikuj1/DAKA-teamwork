b4bfc066d5ba0041bb84d225fdf0deacaa7ff734
janoskii03 <110709363+janoskii03@users.noreply.github.com>
2023-07-16 22:37:45 +0800

    新增一點訂位和修改登入/註冊

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -14,8 +14,9 @@
         <img :src="require('@/assets/images/login/cross.png')" class="close_modal" @click="closeModal">
         <form>
 
-          <input class="login_memid" type="text" name="memId" placeholder="信箱" v-model="memId" @input="reset" require>
-          <input type="text" name="memPsw" maxlength="12" placeholder="密碼" v-model="memPsw" @input="reset" require>
+          <input class="login_memid" type="text" name="memId" placeholder="信箱" v-model="memId" @input="errorMsg = ''"
+            require>
+          <input type="text" name="memPsw" maxlength="12" placeholder="密碼" v-model="memPsw" @input="errorMsg = ''" require>
           <div class="error_message">{{ errorMsg }}</div>
           <div class="login_keep">
             <div class="login_keep_status">
@@ -43,22 +44,22 @@
 
             <div class="inputBox">
               <span>名字</span>
-              <input type="text" class="payment-name-input" required v-model="nameReg">
+              <input type="text" class="payment-name-input" required="required" v-model="nameReg">
               <div class="payment-name"></div>
             </div>
             <div class="inputBox">
               <span>信箱</span>
-              <input type="email" class="payment-email-input" required v-model="emailReg">
+              <input type="email" class="payment-email-input" v-model="emailReg" required="required"/>
               <div class="payment-email"></div>
             </div>
             <div class="inputBox">
               <span>密碼</span>
-              <input type="text" class="payment-address-input" required v-model="pswReg">
+              <input type="text" class="payment-address-input" v-model="pswReg"  required="required"/>
               <div class="payment-address"></div>
             </div>
             <div class="inputBox">
               <span>再次輸入密碼</span>
-              <input type="text" class="payment-nation-input" required v-model="pswConfirmReg">
+              <input type="text" class="payment-nation-input" required="required" v-model="pswConfirmReg">
               <div class="payment-nation"></div>
             </div>
 
@@ -75,25 +76,25 @@
             </div>
             <div class="inputBox">
               <span>生日</span>
-              <input type="date" class="payment-card-name-input" required v-model="birthReg">
+              <input type="date" class="payment-card-name-input" v-model="birthReg"  min="1900-01-01" max="2021-01-01" required="required">
             </div>
             <div class="inputBox">
               <span>連絡電話</span>
-              <input type="tel" class="payment-card-number-input" maxlength="12" required v-model="telReg">
+              <input type="tel" class="payment-card-number-input" maxlength="12" required="required" v-model="telReg">
             </div>
           </div>
         </div>
         <div class="register_form_bottom">
           <div class="">
-            <input type="checkbox" name="membership" id="membership">
+            <input type="checkbox" name="membership" id="membership" required="required">
             <label for="membership">我已詳閱並同意<a href="" target="_blank">會員條款</a>與<a href=""
                 target="_blank">隱私權規定</a></label>
           </div>
           <div class="">
-            <input type="checkbox" name="news_daka" id="news_daka">
+            <input type="checkbox" name="news_daka" id="news_daka" required="required">
             <label for="news_daka">我願意收到打咖DAKA的最新消息</label>
           </div>
-          <button class="register_submit" @click.prevent="columnCheck">註冊會員</button>
+          <input class="register_submit" @click="columnCheck" type="submit" value="註冊會員">
         </div>
 
 
@@ -107,9 +108,9 @@
       <p>請輸入您的註冊信箱，進行密碼變更</p>
       <div class="forget_password_enter">
         <label for="">請輸入您的信箱</label>
-        <input type="text" placeholder="信箱" v-model="memEmail">
+        <input type="email" placeholder="信箱" v-model="memEmail" required="required" >
       </div>
-      <button @click.prevent="checkEmail">下一步</button>
+      <input @click="checkEmail" type="submit" value="下一步" class="forget_password_submit">
     </section>
 
     <section class="enter_valid" v-if="forgetPsw && step === 2">
@@ -117,17 +118,17 @@
       <h2>輸入驗證碼</h2>
       <p>已發送驗證碼至sm********@gmail.com</p>
       <div class="enter_valid_input">
-        <input type="number" required v-model="number1" min="0" max="9">
-        <input type="number" required v-model="number2" min="0" max="9">
-        <input type="number" required v-model="number3" min="0" max="9">
-        <input type="number" required v-model="number4" min="0" max="9">
+        <input type="text" required="required" v-model="number1"  max-length="1">
+        <input type="text" required="required" v-model="number2"  max-length="1">
+        <input type="text" required="required" v-model="number3"  max-length="1">
+        <input type="text" required="required" v-model="number4"  max-length="1">
       </div>
       <div class="enter_valid_re">
         <p>10 分鐘內若未收到驗證碼</p>
         <p>請<a href="">按此</a>重新發送</p>
       </div>
 
-      <button @click.prevent="validCheck">送出</button>
+      <input @click="validCheck" type="submit" value="送出" class="enter_valid_submit">
     </section>
 
     <section class="enter_modify" v-if="forgetPsw && step === 3">
@@ -136,14 +137,11 @@
       <p>請輸入 6 -12 位包含英文及數字的密碼</p>
       <div class="enter_modify_input">
         <label for="modifyPsw">新密碼</label>
-        <input type="text" required v-model="number1" maxlength="12" minlength="6" id="modifyPsw">
+        <input type="text" required="required" v-model="modify.psw" maxlength="12" minlength="6" id="modifyPsw">
         <label for="modifyNewPsw">再次輸入新密碼</label>
-        <input type="text" required v-model="number3" maxlength="12" minlength="6" id="modifyNewPsw">
-
+        <input type="text" required="required" v-model="modify.newPsw" maxlength="12" minlength="6" id="modifyNewPsw">
       </div>
-
-
-      <button @click.prevent="modifyCheck">送出</button>
+      <input @click="modifyCheck" type="submit" value="送出" class="enter_modify_submit"> 
     </section>
 
     <section class="enter_modify_success" v-if="forgetPsw && step === 4">
@@ -151,7 +149,15 @@
       <img :src="require('@/assets/images/footerLogo.png')" alt="" class="enter_modify_success_logo">
       <p>修改完成！</p>
       <p>請重新登入</p>
-      <button @click.prevent="modifySuccess">返回會員登入</button>
+      <button @click="modifySuccess">返回會員登入</button>
+    </section>
+
+    <section class="enter_modify_success" v-if="step === 5">
+      <img :src="require('@/assets/images/login/cross.png')" class="modify_close_success_modal" @click="closeModal">
+      <img :src="require('@/assets/images/footerLogo.png')" alt="" class="enter_modify_success_logo">
+      <p>註冊完成！</p>
+      <p>請重新登入</p>
+      <button @click="registerSuccess">返回會員登入</button>
     </section>
 
   </div>
@@ -170,6 +176,7 @@ export default {
       isRegistered: false,
       forgetPsw: false,
       step: 0,
+      errorMsg: '',
       register: {
         nameReg: '',
         emailReg: '',
@@ -178,7 +185,7 @@ export default {
         sexReg: '',
         birthReg: '',
         telReg: '',
-        errorMsg: '',
+
       },
       verification: {
         number1: '',
@@ -219,8 +226,9 @@ export default {
       if (this.memId === 'test' && this.memPsw === 'test') {
         window.alert('登入成功');
         loginStatus = true;
-      } else {
-        this.register.errorMsg = '帳號或密碼輸入錯誤';
+      } 
+      else {
+        this.errorMsg = '帳號或密碼輸入錯誤';
       }
       this.register.memId = '';
       this.register.memPsw = '';
@@ -233,17 +241,20 @@ export default {
 
     },
     columnCheck() {
-      if (this.register.nameReg !== '' && this.register.emailReg !== '' && this.register.pswReg !== '' && this.register.pswConfirmReg !== '' && this.register.birthReg !== '' && this.register.sexReg !== '' && this.register.telReg !== '') {
-        if (this.register.pswReg === this.register.pswConfirmReg && this.register.pswReg.length >= 6 && this.register.pswReg.length <= 12) {
-          console.log('註冊成功');
-          this.isRegistered = false;
-
-        } else {
-          console.log('密碼輸入有誤');
-        }
-      } else {
-        window.alert('請完整輸入');
-      }
+      // if (!this.register.nameReg || !this.register.emailReg || !this.register.pswReg || !this.register.pswConfirmReg || !this.register.birthReg || !this.register.sexReg || !this.register.telReg) {
+      //    alert("請完整輸入");
+      //    return;
+      //   }
+      //   else if (this.register.pswReg === this.register.pswConfirmReg) {
+      //     this.isRegistered = false;
+      //     this.step = 5;
+      //   }
+if(this.register.pswReg === this.register.pswConfirmReg && this.register.pswReg && this.register.pswConfirmReg){
+  
+  this.isRegistered = false;
+  this.step = 5;
+}
+      
     },
     forgetPassword() {
       this.forgetPsw = true;
@@ -253,32 +264,48 @@ export default {
     checkEmail() {
 
       if (!this.memEmail) {
-        alert("輸入錯誤或無輸入");
-      } else {
+        return alert("輸入錯誤或無輸入");
+      } else 
+      
+      {
         this.step = 2;
+        this.memEmail = '';
       }
+
+      
     },
     validCheck() {
-if(!this.number1 && !this.number2 && !this.number3 && !this.number4){
-  alert("請輸入驗證碼");
-}else{
-  this.step = 3;
-}
+      if (!this.number1 || !this.number2 || !this.number3 || !this.number4 || typeof(this.number1)!=Number) {
+        alert("請輸入驗證碼");
+      } else {
+        this.number1 = this.number2 = this.number3 = this.number4 = '';
+        this.step = 3;
+      }
 
     },
     modifyCheck() {
-      if (this.modify.psw === this.modify.newPsw && !this.modify.psw && !this.modify.newPswpsw) {
-        this.step = 4;
-        this.modify.psw='';
-        this.modify.newPsw='';
-
-      } else {
-        alert("不一樣")
+      if (!this.modify.psw || !this.modify.newPsw) {
+        alert("請輸入密碼");
+        return;
       }
+      else if ((this.modify.psw === this.modify.newPsw && this.modify.psw.length>=6 &&this.modify.psw.length<=12)) {
+        this.step = 4;
+        this.modify.psw = this.modify.newPsw = '';
+        return;
+      } else {
+        alert("請輸入密碼");
+      }
+
+      this.modify.psw = '';
+      this.modify.newPsw = '';
     },
     modifySuccess() {
       this.forgetPsw = false;
       this.step = 0;
+    },
+    registerSuccess() {
+      this.step = 0;
+
     }
 
   }
@@ -286,8 +313,6 @@ if(!this.number1 && !this.number2 && !this.number3 && !this.number4){
 
 </script>
 
-<script>
 
-</script>
 
 
