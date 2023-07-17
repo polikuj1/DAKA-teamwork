d56cbec08028fd78503033fcf0166897d88027ab
張嘉哲 <polikuj1@yahoo.com.tw>
2023-07-17 12:10:31 +0800

    漫畫卡片加入購物車完成

--- src/components/comic/CartIcon.vue
+++ src/components/comic/CartIcon.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="cart_icon">
     <div class="wrap">
+      <div class="cart_info" v-show="infoOpen">
+        <button @click="infoOpen = false"><i class="fa-solid fa-xmark"></i></button>
+        <span v-if="bookData.length === 0">您還沒有預約任何一本書， 請開始 <a href="">預約</a> 吧！</span>
+        <div class="reservation_list" v-else>
+          <ul>
+            <li v-for="(book,index) in bookData" :key="book.title">
+              <div>
+                <span>{{ book.title }} {{ book.index }}</span>
+                <span>{{ book.author }}</span>
+              </div>
+              <span @click="deleteBook(index)"><i class="fa-solid fa-trash-can"></i></span>
+            </li>
+          </ul>
+          <button>前往預約</button>
+        </div>
+      </div>
       <div class="icon" @click="infoOpen = true">
         <span><img src="@/assets/images/comic/book.svg" alt="bool"></span>
         <img src="@/assets/images/home/circle04.png" alt="circle">
-      </div>
-      <div class="cart_info" v-show="infoOpen">
-        <button @click="infoOpen = false"><i class="fa-solid fa-xmark"></i></button>
-        <span>您還沒有預約任何一本書， 請開始 <a href="">預約</a> 吧！</span>
       </div>
     </div>
   </div>
@@ -15,15 +27,17 @@
 
 <script>
 export default {
+  props: ['bookData'],
   data() {
     return {
       infoOpen: false,
+      bookList: [],
     }
   },
   methods: {
-    // checkList() {
-    //   this.infoOpen = true;
-    // }
+    deleteBook(index) {
+      this.bookData.splice(index,1);
+    },
   },
 }
 </script>
--- src/components/comic/ComicCard.vue
+++ src/components/comic/ComicCard.vue
@@ -3,36 +3,38 @@
   <div class="search_result" v-show="search">
     &ldquo;{{ search }}&rdquo; 共有 {{ comicData.length }} 筆符合的漫畫
   </div>
-  <main class="wrap">
+  <div class="comic_card_wrap">
     <div class="circle_top">
       <div></div>
     </div>
     <div class="circle_bottom">
       <div></div>
     </div>
-    <div class="card" v-for="item in comicData" :key="item.title" 
-    v-show="comicData.length">
-      <div class="pic"  @click="this.$router.push('/comic_detail')">
-        <img :src="item.img">
-      </div>
-      <div class="txt">
-        <div class="title">
-          <h4>{{ item.title }} {{ item.index }}</h4>
-          <span>{{ item.author }}</span>
+    <main class="wrap">
+      <div class="card" v-for="item in comicData" :key="item.title"
+      v-show="comicData.length">
+        <div class="pic"  @click="this.$router.push('/comic_detail')">
+          <img :src="item.img">
         </div>
-        <button @click="reserve(item)">預約此書 <i class="fa-solid fa-book-open-reader"></i></button>
+        <div class="txt">
+          <div class="title">
+            <h4>{{ item.title }} {{ item.index }}</h4>
+            <span>{{ item.author }}</span>
+          </div>
+          <button @click="reserve(item)">預約此書 <i class="fa-solid fa-book-open-reader"></i></button>
+        </div>
       </div>
-    </div>
-    <div class="warn_txt" v-if="comicData.length === 0">
-      請重新搜尋!!
-    </div>
-  </main>
-  <CartIcon />
+      <div class="warn_txt" v-if="comicData.length === 0">
+        請重新搜尋!!
+      </div>
+    </main>
+  </div>
+  <CartIcon :book-data="reservation"/>
 </template>
 
 <script>
-import comicSearch from '@/components/comic/search.vue';
 import comicData from '@/assets/js/comicInfo.js';
+import comicSearch from '@/components/comic/search.vue';
 import CartIcon from '@/components/comic/CartIcon.vue';
 export default {
   components: {
@@ -53,6 +55,7 @@ export default {
       this.comicData = comicData.filter(item => item.title.includes(this.search));
     },
     reserve(item) {
+      if(this.reservation.length === 5) return;
       this.reservation.push(item);
     },
   },
