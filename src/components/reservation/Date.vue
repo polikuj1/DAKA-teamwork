ff15be0b3c08b757e15a6fc4bbe286990fcea1d5
janoskii03 <ilovenikeya@gmail.com>
2023-07-17 21:37:32 +0800

    加了套件，css有點跑掉

---
+++ src/components/reservation/Date.vue
@@ -0,0 +1,35 @@
+<template>
+    <Space size="large" wrap class="reservation_date">
+        <DatePicker type="date" placeholder="Select date" v-model="selectedDate" />
+        <TimePicker type="timerange" placement="bottom-end" placeholder="Select time" style="width: 168px" v-model="selectedTime"
+        />
+   {{ selectedDate }}, {{ selectedTime }}
+      </Space>
+
+
+</template>
+<style>
+.reservation_date{
+  display: flex;
+   
+}
+</style>
+<script>
+export default {
+data(){
+  return{
+    selectedDate:'',
+    selectedTime:'',
+  }
+},
+computed(){
+  
+
+},
+methods:{
+convertDate(){
+  this.$emit(selectedDate);
+}
+}
+}
+</script>
--- src/views/ReservationView.vue
+++ src/views/ReservationView.vue
@@ -12,11 +12,16 @@
           <li><img :src="require('@/assets/images/reservation/Vector.svg')" class="reservation_rules_img">
             為利於其餘客戶權益，每次預約僅保留30分鐘，逾時未到視同放棄，
             如需取消預約，請於1日前取消。</li>
-          <li><img :src="require('@/assets/images/reservation/Vector.svg')" class="reservation_rules_img">系統僅提供座位消費金額作為參考，實際消費金額將以當日使用情況為主，最終金額可能因餐點選擇、特殊要求或其他因素而有所變動。</li>
-          <li><img :src="require('@/assets/images/reservation/Vector.svg')" class="reservation_rules_img">目前線上付款僅提供扣除儲值金。</li>
-          <li><img :src="require('@/assets/images/reservation/Vector.svg')" class="reservation_rules_img">我司將不會以簡訊方式另行通知，如有疑慮，請撥打聯絡電話。</li>
-          <li><img :src="require('@/assets/images/reservation/Vector.svg')" class="reservation_rules_img">我司保有最終修改、變更、活動解釋及取消本活動之權利，若有相關異動將會公告於網站， 恕不另行通知。</li>
-          <li><img :src="require('@/assets/images/reservation/Vector.svg')" class="reservation_rules_img">座位區預約上限5位，包廂區預約上限2間。</li>
+          <li><img :src="require('@/assets/images/reservation/Vector.svg')"
+              class="reservation_rules_img">系統僅提供座位消費金額作為參考，實際消費金額將以當日使用情況為主，最終金額可能因餐點選擇、特殊要求或其他因素而有所變動。</li>
+          <li><img :src="require('@/assets/images/reservation/Vector.svg')" class="reservation_rules_img">目前線上付款僅提供扣除儲值金。
+          </li>
+          <li><img :src="require('@/assets/images/reservation/Vector.svg')"
+              class="reservation_rules_img">我司將不會以簡訊方式另行通知，如有疑慮，請撥打聯絡電話。</li>
+          <li><img :src="require('@/assets/images/reservation/Vector.svg')"
+              class="reservation_rules_img">我司保有最終修改、變更、活動解釋及取消本活動之權利，若有相關異動將會公告於網站， 恕不另行通知。</li>
+          <li><img :src="require('@/assets/images/reservation/Vector.svg')"
+              class="reservation_rules_img">座位區預約上限5位，包廂區預約上限2間。</li>
         </ul>
 
 
@@ -25,12 +30,52 @@
 
     </section>
 
-<section class="reservation_date">
-  <h2>
-        <span>2</span> 選擇日期及時間
-      </h2>
-      
+    <section class="reservation_date">
+      <h2> <span>2</span> 選擇日期及時間</h2>
+      <Date></Date>
     </section>
+
+
+    <section class="reservation_confirm">
+      <h2>
+        <span>4</span> 確認預約細項
+      </h2>
+      <div class="reservation_confirm_data">
+        <div class="confirm_data_time">
+          <div class="data_time_start">
+
+            <p for="">預約開始時間</p>
+            <input type="text" readonly value="April  25, 2023  10:00 PM">
+          </div>
+          <div class="data_time_end">
+
+            <p for="">預約結束時間</p>
+            <input type="text" readonly value="April  25, 2023  08:00 AM">
+          </div>
+          <div class="data_time_sum">
+
+            <p for="">金額總計</p>
+            <input type="text" readonly>
+          </div>
+          <div class="data_time_stored">
+
+            <p for="">目前儲值金</p>
+            <input type="text" readonly>
+            <div class="stored_error">儲值金不足，請先至 <a href="">會員中心</a>
+               儲值。</div>
+          </div>
+        </div>
+
+        <div class="confirm_data_seat">
+          <p for="">選定座位</p>
+            <input type="text" readonly>
+          
+        </div>
+
+      </div>
+      <button class="reservation_submit">確認預約</button>
+    </section>
+
   </main>
 
 
@@ -49,17 +94,14 @@
 
 <script>
 import PageTitle from '@/components/PageTitle.vue';
-
+import Date from '@/components/reservation/Date.vue';
 
 
 export default {
   components: {
-    PageTitle
+    PageTitle, Date
   },
   data() {
-    components: {
-
-    }
     return {
       title: '訂位預約',
     }
