510aaf784656e42fca33cc421f892c7ff2cd5ed1
kkkzhenyiii <kangzhenyi77@gmail.com>
2023-07-19 21:41:29 +0800

    '新增首頁餐點區塊'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 d<template>
   <div class="home">
     <Services></Services>
+    <Meals/>
   </div>
 </template>
 
 <script>
 import Services from '@/components/home/Services.vue';
+import Meals from '@/components/home/Meal.vue';
 export default {
   name: '',
   data() {
@@ -20,7 +22,7 @@ export default {
 
   },
   components: {
-    Services,
+    Services, Meals,
   },
   mounted() {
     
