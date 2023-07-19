598dc0b7e561c8f0d4c7d39a40e18e5a24ad158f
ioop987 <ioop987987@gmail.com>
2023-07-19 14:41:04 +0800

    add news section

--- src/components/home/news.vue
+++ src/components/home/news.vue
@@ -1,16 +1,48 @@
 <template>
     <section class="news">
-        <div class="a">2023.05.01!!  五月會員好康禮</div>
+        <a class="infor" v-for="item in news" :key="item.text">
+            <div class="category">{{ item.category }}</div>
+            <div class="date">{{ item.date }}</div>
+            <div class="text">{{ item.text }}</div>
+            <img class="pic" src="{{item.pic}}" alt="箭頭">
+        </a>
 
 
     </section>
 </template>
 
 <script>
-export default{
+export default {
     data() {
         return {
-            
+            news: [
+                {
+                    category: 'NEWS',
+                    date: '2023.05.01',
+                    text: '五月會員好康禮',
+                    pic: ''
+                },
+
+                {
+                    category: 'ACTICIVY',
+                    date: '2023.05.01',
+                    text: '五月會員好康禮',
+                    pic: ''
+                },
+
+                {
+                    category: 'ACTICIVY',
+                    date: '2023.05.01',
+                    text: '五月會員好康禮',
+                    pic: ''
+                },
+
+                {
+                    category: 'NEWS',
+                    date: '2023.05.01',
+                    text: '五月會員好康禮',
+                    pic: ''
+                }],
         }
     },
 }
