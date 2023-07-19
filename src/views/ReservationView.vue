00601e7c1d413116d5a485f82ce24fc8e5e2690e
janoskii03 <ilovenikeya@gmail.com>
2023-07-19 19:36:07 +0800

    處理衝突

--- src/views/ReservationView.vue
+++ src/views/ReservationView.vue
@@ -13,41 +13,6 @@
               class="reservation_rules_img"
             />
             為利於其餘客戶權益，每次預約僅保留30分鐘，逾時未到視同放棄，
-<<<<<<< HEAD
-            如需取消預約，請於1日前取消。
-          </li>
-          <li>
-            <img
-              :src="require('@/assets/images/reservation/Vector.svg')"
-              class="reservation_rules_img"
-            />系統僅提供座位消費金額作為參考，實際消費金額將以當日使用情況為主，最終金額可能因餐點選擇、特殊要求或其他因素而有所變動。
-          </li>
-          <li>
-            <img
-              :src="require('@/assets/images/reservation/Vector.svg')"
-              class="reservation_rules_img"
-            />目前線上付款僅提供扣除儲值金。
-          </li>
-          <li>
-            <img
-              :src="require('@/assets/images/reservation/Vector.svg')"
-              class="reservation_rules_img"
-            />我司將不會以簡訊方式另行通知，如有疑慮，請撥打聯絡電話。
-          </li>
-          <li>
-            <img
-              :src="require('@/assets/images/reservation/Vector.svg')"
-              class="reservation_rules_img"
-            />我司保有最終修改、變更、活動解釋及取消本活動之權利，若有相關異動將會公告於網站，
-            恕不另行通知。
-          </li>
-          <li>
-            <img
-              :src="require('@/assets/images/reservation/Vector.svg')"
-              class="reservation_rules_img"
-            />座位區預約上限5位，包廂區預約上限2間。
-          </li>
-=======
             如需取消預約，請於1日前取消。</li>
           <li><img :src="require('@/assets/images/reservation/Vector.svg')" class="reservation_rules_img">系統僅提供座位消費金額作為參考，實際消費金額將以當日使用情況為主，最終金額可能因餐點選擇、特殊要求或其他因素而有所變動。</li>
           <li><img :src="require('@/assets/images/reservation/Vector.svg')" class="reservation_rules_img">目前線上付款僅提供扣除儲值金。</li>
@@ -162,4 +127,3 @@ export default {
 }
 
 </script>
->>>>>>> 3eaa45e02ae16209747aac33879bdb98cfea93db
