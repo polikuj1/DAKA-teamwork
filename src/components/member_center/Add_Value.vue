510459bb1cb287f030631706cb6517f8db0d85d7
hazelshen <smile351293@gmail.com>
2023-07-20 16:42:51 +0800

    新增線上儲值

--- src/components/member_center/Add_Value.vue
+++ src/components/member_center/Add_Value.vue
@@ -1,7 +1,4 @@
 <template>
-  <PageTitle>
-      {{ title }}
-  </PageTitle>
   <div id="member_charge">
     <div class="online_charge">
       <span class="charge_icon"><i class="fa-solid fa-circle-chevron-left"></i></span>
@@ -9,74 +6,77 @@
     </div>
 
     <form action="" class="charge_content">
-      <label for="amount" class="charge_amount">選擇金額:</label>
+      <label for="amount" class="charge_amount">選擇金額</label>
       <select id="amount" class="amount_option" v-model="selectedAmount" required>
-        <option v-for="amount in amounts" :value="amount">{{ amount }}</option>
+        <option value="請選擇" disabled selected>請選擇</option>
+        <option v-for="amount in amounts" :key="amount" :value="amount">{{ amount }}</option>
       </select>
       <br>
-      <label for="creditCard" class="charge_card">選擇扣款信用卡:</label>
+      <label for="creditCard" class="charge_card">選擇扣款信用卡</label>
       <select id="creditCard" class="card_option" v-model="selectedCreditCard" required>
-        <option v-for="card in boundCreditCards" :value="card">{{ card }}</option>
+        <option value="請選擇" disabled selected>請選擇</option>
+        <option v-for="card in boundCreditCards" :key="card" :value="card">{{ card }}</option>
+        <option value="其他">其他</option>
       </select>
       <br>
-      <label for="cardNumber" class="charge_cardnumber">輸入卡號:</label>
-      <input type="number" id="cardNumber" name="cardNumber" required>
-      <br>
-      <label for="expiryDate">輸入卡片有效期限:</label>
+      <div v-if="selectedCreditCard === '其他'">
+        <label for="cardNumber" class="charge_cardnumber">輸入卡號：</label>
+        <input type="text" id="cardNumber" name="cardNumber" v-model="cardNumber" required maxlength="16" pattern="[0-9]*">
+        <br>
+        <label for="expiryDate">輸入卡片有效期限</label>
         <div class="charge_goodthru">
           <select v-model="selectedYear" required>
-            <option value="">選擇年份</option>
-            <option v-for="year in years" :value="year">{{ year }}</option>
+            <option value="請選擇" disabled selected>請選擇年份</option>
+            <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
           </select>
           <select v-model="selectedMonth" required>
-            <option value="">選擇月份</option>
-            <option v-for="month in months" :value="month">{{ month }}</option>
+            <option value="請選擇" disabled selected>請選擇月份</option>
+            <option v-for="month in months" :key="month" :value="month">{{ month }}</option>
           </select>
         </div>
-      <br>  
-      <label for="verificationCode">輸入驗證碼3碼:</label>
-      <input type="number" id="verificationCode" name="verificationCode" required>
+        <label for="verificationCode">輸入驗證碼3碼</label>
+        <input type="text" id="verificationCode" name="verificationCode" required maxlength="3" pattern="[0-9]*">
+        <br>
+        <div class="charge_bind">
+          <input type="checkbox" id="isBound" value="true" v-model="isBound">
+          <label for="isBound">是否綁定此信用卡</label>
+        </div>
+        <div class="charge_default">
+          <input type="checkbox" id="isdefault" value="true" v-model="isDefault">
+          <label for="isdefault">是否將此信用卡設為預設卡片</label>
+        </div>
+      </div>
+      <br>
+      <button type="button" @click="submit">送出</button>
     </form>
-    
-
-    <div class="charge_bind">
-      <input type="checkbox" id="isBound" value="true" v-model="isBound" required>
-      <label for="bind">是否綁定此信用卡</label>
-    </div>
-    <div class="charge_default">
-      <input type="checkbox" id="default" value="true" v-model="isDefault" required>
-      <label for="default">是否將此信用卡設為預設卡片</label>
-    </div>
-      
-    <button type="button" onclick="simulateSuccess()">送出</button>
-
   </div>
-
 </template>
 
 <script>
 import PageTitle from '@/components/PageTitle.vue';
 export default {
   components: {
-    PageTitle
+    
   },
   data() {
     return {
       title: '會員中心',
-      selectedAmount: '',
-      amounts: ['100', '500', '1000', '5000'], // 假資料 - 金額選項
-      selectedCreditCard: '', // 初始化選擇的信用卡為空
-      boundCreditCards: ['中國信託LINE聯名卡', '富邦J卡', 'Richart'], // 假資料 - 綁定信用卡列表
+      selectedAmount: '請選擇',
+      amounts: ['100', '500', '1000', '5000'], 
+      selectedCreditCard: '請選擇', // 初始化選擇的信用卡為空
+      boundCreditCards: ['中國信託LINE聯名卡', '富邦J卡', 'Richart'],
       cardNumber: '', // 輸入的卡號
-      selectedYear: ['2026','2025','2024','2023','2022','2021','2020','2019'], // 初始化選擇的年份為空
-      selectedMonth: ['1','2','3','4','5','6','7','8','9','10','11','12'], // 初始化選擇的月份為空
+      selectedYear: '請選擇',
+      years: ['2030','2029','2028','2027','2026','2025','2024','2023'],
+      selectedMonth: '請選擇',
+      months: ['01','02','03','04','05','06','07','08','09','10','11','12'],
       verificationCode: '', // 輸入的驗證碼
       isBound: false, // 是否綁定此信用卡
       isDefault: false, // 是否將此信用卡設為預設卡片
     }
   },
   methods: {
-    simulateSuccess() {
+    submit() {
       // 獲取填寫的假資料和選擇的信用卡資訊
       const amount = this.selectedAmount;
       const selectedCreditCard = this.selectedCreditCard;
