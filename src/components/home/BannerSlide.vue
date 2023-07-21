6d4351691041e92c4bea6ff44cec2b6582873dd0
kkkzhenyiii <kangzhenyi77@gmail.com>
2023-07-21 16:03:02 +0800

    home_meal RWD & banner section

--- src/components/home/BannerSlide.vue
+++ src/components/home/BannerSlide.vue
@@ -1,24 +1,52 @@
 <template>
-    1111
-    <carousel>
-        <slide>
-            Slide 1 Content
-        </slide>
-        <slide>
-            Slide 2 Content
-        </slide>
-    </carousel>
+    <Carousel :autoplay="2000" :wrap-around="true">
+        <Slide v-for="slide in 10" :key="slide">
+            <div class="carousel__item">{{ slide }}</div>
+        </Slide>
+
+        <template #addons>
+            <Pagination />
+        </template>
+    </Carousel>
 </template>
 
 <script>
-// import Vue from 'vue';
-// import VueCarousel from 'vue-carousel';
-// Vue.use(VueCarousel);
-// import { Carousel, Slide } from 'vue-carousel';
-// export default {
-//     components: {
-//         Carousel,
-//         Slide
-//     }
-// };
+import { defineComponent } from 'vue'
+import { Carousel, Pagination, Slide } from 'vue3-carousel'
+
+import 'vue3-carousel/dist/carousel.css'
+
+export default defineComponent({
+    name: 'Autoplay',
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+    },
+})
 </script>
+
+<style>
+.carousel__item {
+    min-height: 200px;
+    height: 500px;
+    width: 100%;
+    background-color: var(--vc-clr-primary);
+    color: var(--vc-clr-white);
+    font-size: 20px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.carousel__slide {
+    padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+    box-sizing: content-box;
+    border: 5px solid white;
+}
+</style>
