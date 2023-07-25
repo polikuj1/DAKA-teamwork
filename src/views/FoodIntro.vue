1417c5066862ee6482a8796ec77c06eb148a57e3
kid5227 <kid52277802@gmal.com>
2023-07-26 00:04:45 +0800

    add food page scss url 230726 1204

--- src/views/FoodIntro.vue
+++ src/views/FoodIntro.vue
@@ -7,7 +7,9 @@
     
   </div>
 </template>
-
+<style>
+@import '@/assets/scss/page/food_intro.scss';
+</style>
 <script>
 import PageTitle from '@/components/PageTitle.vue';
 import PopularMeals from '@/components/food_intro/PopularMeals.vue';
