0afebc0d8e2c79d2c7d9f6ffc07af4ca38462cd8
EllieMon <elliemon2020@gmail.com>
2023-08-07 15:57:35 +0800

    更改前端php網址路徑 DAKA/DAKA_frontend

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,2 +1,2 @@
-export const URL = 'http://localhost/DAKA/public/phpfile';
+export const URL = 'http://localhost/DAKA/DAKA_frontend/public/phpfile';
 // export const URL = 'https://tibamef2e.com/chd102/g6/phpfile';
