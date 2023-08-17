767a2fb7a16acb15e52ffc7fe9592ec38ee54fdb
EllieMon <elliemon2020@gmail.com>
2023-08-17 18:28:10 +0800

    會員座位訂位紀錄抓取資料php

--- public/phpfile/getSeatRecord.php
+++ public/phpfile/getSeatRecord.php
@@ -7,37 +7,43 @@ try {
 	
 	//執行sql指令並取得pdoStatement
   $id = $_GET["id"];
-  $sql = "SELECT 
-  m.mem_id,
-  GROUP_CONCAT(DISTINCT so.seat_order_id) AS seat_order_ids,
-  so.seat_order_no,
-  GROUP_CONCAT(DISTINCT so.seat_order_date) AS seat_order_dates,
-  GROUP_CONCAT(DISTINCT so.seat_order_startdate) AS seat_order_startdates,
-  GROUP_CONCAT(DISTINCT so.seat_order_enddate) AS seat_order_enddates,
-  GROUP_CONCAT(DISTINCT so.seat_order_sum) AS seat_order_sums,
-  GROUP_CONCAT(DISTINCT so.seat_order_state) AS seat_order_states,
-  GROUP_CONCAT(DISTINCT sod.seat_id) AS seat_ids,
-  GROUP_CONCAT(DISTINCT sod.seat_order_detail_id) AS seat_order_detail_ids,
-  GROUP_CONCAT(DISTINCT s.seat_area) AS seat_areas,
-  GROUP_CONCAT(DISTINCT s.seat_number) AS seat_numbers,
-  s.seat_sal
-FROM 
-  member m 
-  JOIN seat_order so ON m.mem_id = so.mem_id
+  $sql = "SELECT so.seat_order_id, so.seat_order_no, so.seat_order_date, so.seat_order_startdate, so.seat_order_enddate, so.seat_order_sum, so.seat_order_state, sod.seat_order_detail_id, sod.seat_id, s.seat_area, s.seat_number, s.seat_sal, m.mem_id
+  FROM seat_order so
   JOIN seat_order_detail sod ON so.seat_order_id = sod.seat_order_id
-  JOIN seat s ON sod.seat_id = s.seat_id
-WHERE 
-  m.mem_id = $id
-GROUP BY 
-  so.seat_order_no,
-  m.mem_id,
-  s.seat_sal
-ORDER BY
-  so.seat_order_state;
-";
+  JOIN seat s on sod.seat_id = s.seat_id
+  join member m on so.mem_id = m.mem_id
+  where so.mem_id = $id 
+  ORDER BY so.seat_order_id desc, so.seat_order_state";
 	$products = $pdo->query($sql); 
 	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
-  echo json_encode($prodRows);
+  $orders = array();
+
+  foreach ($prodRows as $row) {
+      $seat_order_id = $row['seat_order_id'];
+      if (!isset($orders[$seat_order_id])) {
+          // array_push($orders, $row);
+          $orders[$seat_order_id] = array(
+              'seat_order_id' => $seat_order_id,
+              'seat_order_no' => $row['seat_order_no'],
+              'seat_order_state' => $row['seat_order_state'],
+              'seat_order_date' => $row['seat_order_date'],
+              'seat_order_startdate' => $row['seat_order_startdate'],
+              'seat_order_enddate' => $row['seat_order_enddate'],
+              'seat_order_sum' => $row['seat_order_sum'],
+              'seat_order_detail_id' => $row['seat_order_detail_id'],
+              'mem_id' => $row['mem_id'],
+              'items' => array()
+          );
+      }
+      $orders[$seat_order_id]['items'][] = array(
+          'seat_id' => $row['seat_id'],
+          'seat_area' => $row['seat_area'],
+          'seat_number' => $row['seat_number'],
+          'seat_sal' => $row['seat_sal'],
+      );
+  }
+
+  echo json_encode(array_values($orders));
 
 } catch (Exception $e) {
 	echo "錯誤行號 : ", $e->getLine(), "<br>";
--- src/components/member_center/SeatReservation.vue
+++ src/components/member_center/SeatReservation.vue
@@ -1,47 +1,62 @@
 <template>
-    <MbForm @emit-back="triggerParent">
-      <template v-slot:mb_form_title>
-        <h3>{{ title }}</h3>
-      </template>
-      <template v-slot:mb_content>
-        <div class="member_center_res">
-            <div class="reservation_container" v-for="reservation in reservations" :key="reservation.id">
-                <div class="reservation_card">
-                    <div class="reservation_number">
-                        <p>訂位序號</p>
-                        <p>{{ reservation.seat_order_no }}</p>
-                    </div>
-                    <div class="reservation_list">
-                        <div class="reservation_usetime">
-                            <p class="seat_usetime">開台時間</p> 
-                            <p>{{ reservation.seat_order_dates }}<br>{{ reservation.time }}</p>
-                        </div>
-                        <div class="member_reservation_seat">
-                            <p class="seat_number">座位編號</p> 
-                            <p>{{ reservation.seat_areas }}<br>{{ reservation.seat_numbers }}</p>
-                        </div>
-                        <div class="reservation_amount">
-                            <p class="seat_cost">金額</p> 
-                            <p>{{ reservation.seat_order_sums }}</p>
-                        </div>
-                        <div class="reservation_payment">
-                            <p class="seat_pay">付款方式</p> 
-                            <p>儲值金</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="seat_reservation_status">
-                    <div class="reservation_status" :style="statusStyle(reservation.status)">
-                        <p>{{ reservation.seat_order_states }}</p>
-                    </div>
-                    <div v-if="reservation.seat_order_states === '預約中'">
-                        <button @click="cancelReservation(reservation.seat_order_ids)">取消預約</button>
-                    </div>
-                </div>
+  <MbForm @emit-back="triggerParent">
+    <template v-slot:mb_form_title>
+      <h3>{{ title }}</h3>
+    </template>
+    <template v-slot:mb_content>
+      <div class="member_center_res">
+        <div
+          class="reservation_container"
+          v-for="reservation in reservations"
+          :key="reservation.id"
+        >
+          <div class="reservation_card">
+            <div class="reservation_number">
+              <p>訂位序號</p>
+              <p>{{ reservation.seat_order_no }}</p>
             </div>
+            <div class="reservation_list">
+              <div class="reservation_usetime">
+                <p class="seat_usetime">開台時間</p>
+                <p>
+                  {{ reservation.seat_order_dates }}<br />{{ reservation.time }}
+                </p>
+              </div>
+              <div class="member_reservation_seat">
+                <p class="seat_number">座位編號</p>
+                <p>
+                  {{ reservation.seat_areas }}<br />{{
+                    reservation.seat_numbers
+                  }}
+                </p>
+              </div>
+              <div class="reservation_amount">
+                <p class="seat_cost">金額</p>
+                <p>{{ reservation.seat_order_sums }}</p>
+              </div>
+              <div class="reservation_payment">
+                <p class="seat_pay">付款方式</p>
+                <p>儲值金</p>
+              </div>
+            </div>
+          </div>
+          <div class="seat_reservation_status">
+            <div
+              class="reservation_status"
+              :style="statusStyle(reservation.status)"
+            >
+              <p>{{ reservation.seat_order_states }}</p>
+            </div>
+            <div v-if="reservation.seat_order_states === '預約中'">
+              <button @click="cancelReservation(reservation.seat_order_ids)">
+                取消預約
+              </button>
+            </div>
+          </div>
         </div>
-      </template>
-      <template v-slot:mb_modal>
+      </div>
+    </template>
+    <template v-slot:mb_modal>
       <MbModal v-show="modalSwitch">
         <template v-slot:modal_txt>
           <div class="seat_modal">
@@ -51,118 +66,147 @@
           </div>
         </template>
       </MbModal>
-      </template>
-    </MbForm>
-  </template>
-  
-  <script>
+    </template>
+  </MbForm>
+</template>
+
+<script>
 //   import {GET} from '@/plugin/axios';
-  import MbModal from '@/components/member_center/MemberModal.vue';
-  import MbForm from '@/components/member_center/form_style.vue';
-  export default {
-    components: {
-      MbForm, MbModal
-    },
-    data() {
-        return {
-            title: '座位預約紀錄',
-            reservations: [],
-            modalSwitch: false,
-            seat_order_id: null,
-        };
-      
-    },
-    methods: {
-        getData() {
-            const params = {
-                id: this.$store.state.member.mem_id
-            }
-            this.axios.get(`${this.$URL}/getSeatRecord.php`, {params: params})
-                .then((res) => {
-                    console.log(res);
-                    this.reservations = res.data;
-                    // 將座位的代碼轉換成對應的名稱
-                    this.reservations.forEach(item => {
-                        switch (item.seat_areas) {
-                            case 'A':
-                                item.seat_areas = '大廳一般區';
-                                break;
-                            case 'B':
-                                item.seat_areas = '大廳電競區';
-                                break;
-                            case 'C':
-                                item.seat_areas = '包廂單人房';
-                                break;
-                            case 'D':
-                                item.seat_areas = '包廂雙人房';
-                                break;
-                        }
-                        switch (item.seat_order_states) {
-                            case '0':
-                                item.seat_order_states = '預約中';
-                                break;
-                            case '1':
-                                item.seat_order_states = '已完成';
-                                break;
-                            case '2':
-                                item.seat_order_states = '已取消';
-                                break;
-                        }
-                    })
-                })
-                .catch((err) => {
-                console.log(err);
-                })
-        },
-        cancelReservation(reservationId) {
-            // 將要取消的座位訂單編號存下來
-            this.modalSwitch = true;
-            this.seat_order_id = reservationId;
-        },
-        confirmCancel() {
-            this.axios.post(`${this.$URL}/cancelSeatReservation.php`, JSON.stringify({ seat_order_id: this.seat_order_id }) ,{
-                headers: {
-                    'Content-Type': 'text/plain'
-                },
-            })
-                .then(res => {
-                    console.log(res);
-                    this.getData();
-                    alert('取消成功');
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-            this.modalSwitch = false;
-        },
-        preserve() {
-           this.modalSwitch = false;
-           this.$router.push('/member_center/member_seat_reservation');
-        },
-        /*狀態不同背景顏色不同*/
-        statusStyle(status){
-            if(status==='已完成'|| status === '已取消'){
-                return{
-                    backgroundColor:'#F6CE6E',
-                    opacity: '0.5',
-                };
-            }else if(status==='預約中'){
-                return {
-                    backgroundColor: '#F6CE6E',
-                    opacity: '1',
-                };
-            }
-        },
-        triggerParent() {
-            this.$emit('emit-title','');
-        },
+import MbModal from "@/components/member_center/MemberModal.vue";
+import MbForm from "@/components/member_center/form_style.vue";
+export default {
+  components: {
+    MbForm,
+    MbModal
   },
-    computed: {
-        
+  data() {
+    return {
+      seatOrder: [],
+      title: "座位預約紀錄",
+      reservations: [],
+      modalSwitch: false,
+      seat_order_id: null
+    };
+  },
+  mounted() {
+    const params = {
+      id: this.$store.state.member.mem_id
+    };
+    this.axios
+      .get(`${this.$URL}/getSeatRecord.php`, { params: params })
+      .then((res) => {
+        console.log(res);
+        this.seatOrder = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // let a=this.currentTime.substring(10,12)==='下午'?12:0;
+    // let aa=this.currentTime.substring(12,14)
+    // let b = +(aa.includes(':') ? aa.replace(':', '') : aa);
+    // console.log("currentTime",this.currentTime)
+    // console.log("a",a)
+    // console.log("aa",aa)
+    // console.log("b",b)
+    // this.currentTime_hours=a+b;
+    // console.log("currentTime_hours",this.currentTime_hours)
+    console.log("seatOrder", this.seatOrder);
+  },
+  methods: {
+    getData() {
+      //   const params = {
+      //       id: this.$store.state.member.mem_id
+      //   }
+      //   this.axios
+      //     .get(`${this.$URL}/getSeatRecord.php`, { params: params })
+      //     .then((res) => {
+      //       console.log(res);
+      //       this.reservations = res.data;
+      //       將座位的代碼轉換成對應的名稱
+      //       this.reservations.forEach(item => {
+      //           switch (item.seat_areas) {
+      //               case 'A':
+      //                   item.seat_areas = '大廳一般區';
+      //                   break;
+      //               case 'B':
+      //                   item.seat_areas = '大廳電競區';
+      //                   break;
+      //               case 'C':
+      //                   item.seat_areas = '包廂單人房';
+      //                   break;
+      //               case 'D':
+      //                   item.seat_areas = '包廂雙人房';
+      //                   break;
+      //           }
+      //           switch (item.seat_order_states) {
+      //               case '0':
+      //                   item.seat_order_states = '預約中';
+      //                   break;
+      //               case '1':
+      //                   item.seat_order_states = '已完成';
+      //                   break;
+      //               case '2':
+      //                   item.seat_order_states = '已取消';
+      //                   break;
+      //           }
+      //       })
+      //     })
+      //     .catch((err) => {
+      //       console.log(err);
+      //     });
     },
-    created() {
-        this.$emit('emit-title',this.title);
-        this.getData();
+    cancelReservation(reservationId) {
+      // 將要取消的座位訂單編號存下來
+      this.modalSwitch = true;
+      this.seat_order_id = reservationId;
+    },
+    confirmCancel() {
+      this.axios
+        .post(
+          `${this.$URL}/cancelSeatReservation.php`,
+          JSON.stringify({ seat_order_id: this.seat_order_id }),
+          {
+            headers: {
+              "Content-Type": "text/plain"
+            }
+          }
+        )
+        .then((res) => {
+          console.log(res);
+          this.getData();
+          alert("取消成功");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      this.modalSwitch = false;
+    },
+    preserve() {
+      this.modalSwitch = false;
+      this.$router.push("/member_center/member_seat_reservation");
+    },
+    /*狀態不同背景顏色不同*/
+    statusStyle(status) {
+      if (status === "已完成" || status === "已取消") {
+        return {
+          backgroundColor: "#F6CE6E",
+          opacity: "0.5"
+        };
+      } else if (status === "預約中") {
+        return {
+          backgroundColor: "#F6CE6E",
+          opacity: "1"
+        };
+      }
+    },
+    triggerParent() {
+      this.$emit("emit-title", "");
     }
+  },
+  computed: {},
+  created() {
+    this.$emit("emit-title", this.title);
+    this.getData();
   }
-  </script>
+};
+</script>
