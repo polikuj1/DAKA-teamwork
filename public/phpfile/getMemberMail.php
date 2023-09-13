a543b154913ea77c88dce72eb3771f0fea421e1c
janoskii03 <110709363+janoskii03@users.noreply.github.com>
2023-09-13 11:04:31 +0800

    port8080移除

--- public/phpfile/getMemberMail.php
+++ public/phpfile/getMemberMail.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:8080");
+// header("Access-Control-Allow-Origin: http://localhost:8080");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Credentials: true");
